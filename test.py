1592cd2e45dd44a1e048e18dab5ce67e9f809bc9
Krutik Gdhiya <66325627+KrutikGadhiya@users.noreply.github.com>
2020-10-09 18:23:51 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+import cv2
+hand_cascade = cv2.CascadeClassifier('harrcascade/fist.xml')
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+    ret, img = cap.read()
+    A = hand_cascade.detectMultiScale(img, 2, 5)
+
+    for (hx, hy, hw, hh) in A:
+        cv2.rectangle(img, (hx, hy), (hx + hw, hy + hh), (125, 125, 0), 2)
+
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
